67f34433d9e2b5397d34869a36885d744223fa69
jaemin-Yoo <iamjm29@gmail.com>
2022-04-27 18:17:18 +0900

    Design: ToolBar 추가

--- presentation/src/main/java/com/jaemin/presentation/main/MainActivity.kt
+++ presentation/src/main/java/com/jaemin/presentation/main/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.jaemin.presentation.main
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import android.view.View
 import com.jaemin.presentation.R
 import com.jaemin.presentation.databinding.ActivityMainBinding
 import com.jaemin.presentation.extension.viewInflateBinding
@@ -15,28 +17,46 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-
+        initToolBar()
         binding.bottomNavigation.setBottomNavigationListener(object: BottomNavBar.OnBottomNavigationListener{
             override fun onNavigationItemSelected(menuItem: MenuItem?): Boolean {
                 return when(menuItem!!.itemId){
                     R.id.menu_home -> {
+                        binding.toolBar.title = ""
+                        supportActionBar?.setDisplayHomeAsUpEnabled(true)
                         true
                     }
                     R.id.menu_book -> {
+                        binding.toolBar.title = "단어장"
+                        supportActionBar?.setDisplayHomeAsUpEnabled(false)
                         true
                     }
                     R.id.menu_add -> {
+                        binding.toolBar.title = "단어추가"
+                        supportActionBar?.setDisplayHomeAsUpEnabled(false)
                         true
                     }
                     R.id.menu_test -> {
+                        binding.toolBar.title = "테스트"
+                        supportActionBar?.setDisplayHomeAsUpEnabled(false)
                         true
                     }
                     R.id.menu_profile -> {
+                        binding.toolBar.title = "프로필"
+                        supportActionBar?.setDisplayHomeAsUpEnabled(false)
                         true
                     }
                     else -> false
                 }
             }
         })
+    }
+
+    private fun initToolBar() {
+        binding.toolBar.title = ""
+        setSupportActionBar(binding.toolBar) // 툴바 사용
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true) // 뒤로가기 버튼 활성화
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.main_logo) // 뒤로가기 버튼 이미지 수정
     }
 }
