65319e742daa30d0deef91876432915825254d4c
jaemin-Yoo <iamjm29@gmail.com>
2022-05-07 23:58:10 +0900

    Design: Fragment Toolbar 배치

--- presentation/src/main/java/com/jaemin/presentation/HomeFragment.kt
+++ presentation/src/main/java/com/jaemin/presentation/HomeFragment.kt
@@ -5,9 +5,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import com.jaemin.presentation.databinding.FragmentHomeBinding
+import com.jaemin.presentation.extension.viewBinding
 
 class HomeFragment : Fragment() {
+
+    private val binding by viewBinding(FragmentHomeBinding::bind)
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setupUI()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    private fun setupUI(){
+
     }
 }
--- presentation/src/main/java/com/jaemin/presentation/main/MainActivity.kt
+++ presentation/src/main/java/com/jaemin/presentation/main/MainActivity.kt
@@ -2,15 +2,10 @@ package com.jaemin.presentation.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.MenuItem
 import android.widget.PopupMenu
 import androidx.navigation.NavController
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.jaemin.presentation.R
 import com.jaemin.presentation.databinding.ActivityMainBinding
 import com.jaemin.presentation.extension.viewInflateBinding
@@ -27,24 +22,13 @@ class MainActivity : AppCompatActivity() {
         setupUI()
     }
 
-    override fun onSupportNavigateUp() = navController.navigateUp()
-
     private fun setNavController(){
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainer) as NavHostFragment
         navController = navHostFragment.findNavController()
     }
 
     private fun setupUI(){
-        initToolBar()
         setBottomNav()
-    }
-
-    private fun initToolBar() {
-        binding.toolBar.title = ""
-        setSupportActionBar(binding.toolBar) // 툴바 사용
-
-        supportActionBar?.setDisplayHomeAsUpEnabled(true) // 뒤로가기 버튼 활성화
-        supportActionBar?.setHomeAsUpIndicator(R.drawable.main_logo) // 뒤로가기 버튼 이미지 수정
     }
 
     private fun setBottomNav(){
@@ -53,6 +37,5 @@ class MainActivity : AppCompatActivity() {
         val menu = popupMenu.menu
 
         binding.bottomNavigation.setupWithNavController(menu, navController)
-        setupActionBarWithNavController(navController)
     }
 }
